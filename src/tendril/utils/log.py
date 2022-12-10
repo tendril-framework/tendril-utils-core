f20da95afd9a37d10238af8c50b6e546c04266e2
Chintalagiri Shashank <shashank.chintalagiri@gmail.com>
2022-12-11 02:51:41 +0530

    Fix config log level application.

--- src/tendril/utils/log.py
+++ src/tendril/utils/log.py
@@ -54,12 +54,14 @@ from logging import CRITICAL  # noqa
 
 #: The default log level for all loggers created through this module,
 #: unless otherwise specified at the time of instantiation.
-DEFAULT = logging.WARNING
+DEFAULT = logging.INFO
 
 
 def apply_config(config=None):
     if not config:
         from tendril import config
+    global DEFAULT
+    DEFAULT = config.LOG_LEVEL
     logging.root.setLevel(config.LOG_LEVEL)
     logger.configure(handlers=[{"sink": sys.stdout, "serialize": False}])
     create_log_file(config.LOG_PATH, config.JSON_LOGS)
