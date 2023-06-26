6a7263d2cf2ee13f927d55fb25823542a51a8e91
Chintalagiri Shashank <shashank.chintalagiri@gmail.com>
2023-06-26 19:14:06 +0530

    Add Pydantic ORM Model base

--- src/tendril/utils/pydantic.py
+++ src/tendril/utils/pydantic.py
@@ -7,3 +7,11 @@ from pydantic import BaseModel
 class TendrilTBaseModel(BaseModel):
     class Config:
         allow_population_by_field_name = True
+
+
+class TendrilTORMModel(BaseModel):
+    class Config:
+        allow_population_by_field_name = True
+        orm_mode = True
+
+
